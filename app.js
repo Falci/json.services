4d88d7b2c5bc8514ec0834f971c05f535bd6fcb8
falci <falci.exe@gmail.com>
2014-10-05 06:04:13 +0000

    Criado pagina inicial

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
+var site = require('./routes/site');
 var api = require('./routes/api');
 
 var app = express();
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', api);
-app.use('/', routes);
+app.use('/', site);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
